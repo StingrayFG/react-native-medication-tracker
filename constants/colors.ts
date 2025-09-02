1bafa249d8228c8c8453939fb59ad153c3d65657
StingrayFG <54187585+StingrayFG@users.noreply.github.com>
2025-09-02 18:01:36 +0200

    Update the useThemeColor hook & theme colors

---
+++ constants/colors.ts
@@ -0,0 +1,61 @@
+
+
+export const colors = {
+  light: {
+    background: '#fff',
+    box: '#d1d5db', //gray-300
+
+    text: '#111827', //gray-900
+    linkText: '#0369a1', //sky-700
+    buttonText: '#fff',
+    optionText: '#111827', //gray-900
+    inputText: '#111827', //gray-900
+    
+    icon: '#0369a1', //sky-700
+    button: '#0369a1', //sky-700
+    option: '#d1d5db', //gray-300
+    
+    inputInactive: '#6b7280', //gray-500
+    inputActive: '#0369a1', //sky-700
+
+    checkboxInactive: '#6b7280', //gray-500
+    checkboxActive: '#0369a1', //sky-700
+
+    tabIconInactive: '#6b7280', //gray-500
+    tabIconActive: '#0369a1', //sky-700
+  },
+
+  dark: {
+    background: '#fff',
+    box: '#d1d5db', //gray-300
+
+    text: '#0c4a6e', //sky-900
+    linkText: '#0369a1', //sky-700
+    buttonText: '#fff',
+    optionText: '#fff',
+    inputText: '#0c4a6e', //sky-900
+    
+    icon: '#0369a1', //sky-700  
+    button: '#0369a1', //sky-700
+    option: '#0369a1B0', //sky-700
+    
+    inputInactive: 'transparent',
+    inputActive: 'transparent',
+
+    checkboxInactive: '#6b7280', //gray-500
+    checkboxActive: '#0369a1', //sky-700
+    
+    tabIconInactive: '#6b7280', //gray-500
+    tabIconActive: '#0369a1', //sky-700
+  },
+
+  /*dark: {
+    background: '#151718',
+    text: '#ECEDEE',
+    icon: '#9BA1A6',
+    button: 'transparent',
+    input: 'transparent',
+    tabIconDefault: '#9BA1A6',
+    tabIconSelected: tintColorDark,
+  },*/
+};
