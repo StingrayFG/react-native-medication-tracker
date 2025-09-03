cfd5e1563d339a1d062501dd4aa791c7aa95e7d6
StingrayFG <54187585+StingrayFG@users.noreply.github.com>
2025-09-03 16:01:30 +0200

    Update the default layout

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,32 +1,35 @@
 import { Link, Stack } from 'expo-router';
-import { StyleSheet } from 'react-native';
 
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { ThemedText } from '@/components/ui/ThemedText';
+import { ThemedView } from '@/components/ui/ThemedView';
+
 
 export default function NotFoundScreen() {
   return (
     <>
-      <Stack.Screen options={{ title: 'Oops!' }} />
-      <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen does not exist.</ThemedText>
-        <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
+      <Stack.Screen options={{ title: 'Error' }} />
+      <ThemedView style={{
+        padding: 12,
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+
+        <ThemedText 
+        type='title' 
+        style={{textAlign: 'center'}}>
+          This screen does not exist
+        </ThemedText>
+
+        <Link href='/(panels)/(tabs)/schedule' style={{
+          marginTop: 12,
+          paddingVertical: 12,
+        }}>
+          <ThemedText type='link'>Go to home screen</ThemedText>
         </Link>
-      </ThemedView>
+
+      </ThemedView >
     </>
   );
-}
+} 
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
-});
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,9 +2,16 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
 import 'react-native-reanimated';
-
+ 
 import { useColorScheme } from '@/hooks/useColorScheme';
+
+import db from '@/db/client';
+import migrations from '@/drizzle/migrations';
+import backgroundServices from '@/services/backgroundServices';
+import { useMigrations } from 'drizzle-orm/expo-sqlite/migrator';
+
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
@@ -12,6 +19,22 @@ export default function RootLayout() {
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
 
+  //
+  const { success, error } = useMigrations(db, migrations);
+
+  useEffect(() => {
+    if (error) {
+      console.log(success, error)
+    }
+  }, [success, error])
+
+  useEffect(() => {
+    (async () => {
+      await backgroundServices.startBackgroundService();
+    })();
+  }, [])
+
+  //
   if (!loaded) {
     // Async font loading only occurs in development.
     return null;
@@ -20,10 +43,10 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
+        <Stack.Screen name='(panels)' options={{ headerShown: false }}/>
+        <Stack.Screen name='+not-found' />
       </Stack>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
     </ThemeProvider>
   );
 }
